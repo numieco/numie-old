d64cce447d09f6fdd093c665aeeafbe511085c45
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-06-29 05:48:08 +0530

    Add zoom in event listener

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -54,6 +54,12 @@ export default class ContactPage extends React.Component {
     this.handleDescBlur = this.handleDescBlur.bind(this)
   }
 
+  componentDidMount () {
+    document.addEventListener('gesturestart', (e) => {
+      e.preventDefault()
+    })
+  }
+
   validateEmail (email) {
     return (/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))
   }
