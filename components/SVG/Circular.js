6cb317f236a270bc258f2ef08e7f12a7dee87cdf
eugeneross <e@eugeneross.com>
2017-08-08 08:45:09 -0700

    css adjustments, footer rotation, CTA text

--- components/SVG/Circular.js
+++ components/SVG/Circular.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Circular = () => (
 
-  <svg width="490px" height="490px" viewBox="0 0 490 490" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <svg className='circularSvg scale' width="490px" height="490px" viewBox="0 0 490 490" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
       <title>circular</title>
       <desc>Created with Sketch.</desc>
       <defs></defs>
-      <g id="Blog---Home" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" opacity="0.08">
+      <g id="Blog---Home" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" opacity="0.15">
           <g id="Blog---Desktop" transform="translate(-396.000000, -3063.000000)" strokeWidth="2" stroke="#FFFFFF">
               <g id="Group-8" transform="translate(0.000000, 3008.000000)">
                   <g id="circular" transform="translate(391.000000, 50.000000)">
                       <g id="original_Rectangle-14_62615852-Group">
-                          <rect id="duplicate_7_original_Rectangle-14_62615852" transform="translate(250.000000, 250.000000) rotate(308.571429) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
-                          <rect id="duplicate_6_original_Rectangle-14_62615852" transform="translate(250.000000, 250.000000) rotate(257.142857) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
-                          <rect id="duplicate_5_original_Rectangle-14_62615852" transform="translate(250.000000, 250.000000) rotate(205.714286) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
-                          <rect id="duplicate_4_original_Rectangle-14_62615852" transform="translate(250.000000, 250.000000) rotate(154.285714) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
-                          <rect id="duplicate_3_original_Rectangle-14_62615852" transform="translate(250.000000, 250.000000) rotate(102.857143) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
-                          <rect id="duplicate_2_original_Rectangle-14_62615852" transform="translate(250.000000, 250.000000) rotate(51.428571) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
-                          <rect id="duplicate_1_original_Rectangle-14_62615852" x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
+                          <rect id="square" transform="translate(250.000000, 250.000000) rotate(308.571429) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
+                          <rect id="square" transform="translate(250.000000, 250.000000) rotate(257.142857) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
+                          <rect id="square" transform="translate(250.000000, 250.000000) rotate(205.714286) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
+                          <rect id="square" transform="translate(250.000000, 250.000000) rotate(154.285714) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
+                          <rect id="square" transform="translate(250.000000, 250.000000) rotate(102.857143) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
+                          <rect id="square" transform="translate(250.000000, 250.000000) rotate(51.428571) translate(-250.000000, -250.000000) " x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
+                          <rect id="square" x="73.1906117" y="73.1906117" width="353.618777" height="353.618777" rx="13.5135135"></rect>
                       </g>
                   </g>
               </g>
