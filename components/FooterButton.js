54af2a4a23091a9760a8ba163bcaca147612b97e
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-08-10 02:39:48 +0530

    use .btn class in footerButton

--- components/FooterButton.js
+++ components/FooterButton.js
@@ -3,14 +3,14 @@ import React from 'react'
 const FooterButton = (props) => {
   if (props.footerSmallText !== null) {
     return (
-        <div className={'footer-button footer-small-text ' + props.smallClassName}>
+        <div className={'footer-button btn ' + props.smallClassName}>
           <span>{props.footerSmallText}</span>
         </div>
       )
   }
   if (props.footerLargeText !== null) {
     return (
-        <div className={'footer-button footer-large-text ' + props.largeClassName}>
+        <div className={'footer-button btn ' + props.largeClassName}>
           <span>{props.footerLargeText}</span>
         </div>
       )
