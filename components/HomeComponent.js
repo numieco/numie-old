0eaa7bbb3e87d8fba33a6b10bcef2e397e5aabf5
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-08-29 06:07:15 +0530

    Add project section design in home page

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -4,6 +4,7 @@ import Footer from './Footer'
 import FooterButton from './FooterButton'
 import CallToAction from './CallToAction'
 import SinglePost from './SinglePost'
+import Looper from './SVG/Looper'
 
 export default class HomeComponent extends React.Component {
   render () {
@@ -105,6 +106,37 @@ export default class HomeComponent extends React.Component {
               smallClassName='learn-more'
               onClick={() => console.log('footerbutton')}
             />
+          </div>
+
+          <div className='project-preview'>
+            <div className='bg-looper'>
+              <Looper />
+            </div>
+            <div className='project clique-meet'>
+              <div className='project-showcase'>
+                <div className='section image-group-1'>
+                  <img src='static/images/projects/clique-meet-1.png' />
+                  <img src='static/images/projects/clique-meet-2.png' />
+                </div>
+                <div className='section image-group-2'>
+                  <img src='static/images/projects/clique-meet-3.png' />
+                  <img src='static/images/projects/clique-meet-4.png' />
+                </div>
+              </div>
+              <div className='project-intro'>
+                <div className='title'>
+                  Clique Meet
+                </div>
+                <span className='tech'>
+                  UI / UX + BRAND + WEB
+                </span>
+                <div className='description'>
+                  Numie is a full service design studio that provides end-to-end design,
+                  development and marketing for you or your company.
+                  We’re in this with you until the end.
+                </div>
+              </div>
+            </div>
           </div>
 
           <Footer />
