fee558cb113162652b6dfd7f7bc7de8a13b5f964
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-09-05 22:11:46 +0530

    Adjust fade in-out project section - home page

--- static/scripts/revealProjects.js
+++ static/scripts/revealProjects.js
@@ -3,33 +3,21 @@ var currentLocation = window.scrollY
 
 window.onscroll = () => {
   document.querySelector('.bg-looper > svg').style.transform = 'rotate(' + (window.scrollY / 180 / (Math.PI * 2)) + 'rad)'
-  // console.log(document.querySelector('.project-preview').offsetTop - window.scrollY)
 }
 
 if (window.innerWidth >= 992) {
 
-window.onload = () => {
-  var arr =  document.getElementsByClassName('project-details')
-  var projectPreview = document.querySelector('.project-preview')
-  var height = projectPreview.clientHeight
-  // height += (1000 * arr.length)
-  // projectPreview.style.height = height + 'px'
+  window.onload = () => {
+    var arr =  document.getElementsByClassName('project-details')
+    var projectPreview = document.querySelector('.project-preview')
+    var height = projectPreview.clientHeight
 
+    $('.project').fixer()
 
-    $('.project').fixer({
-      isFixed: function () {
-        console.log('check')
-      }
-    })
-
-     for (var i=0; i<arr.length; i++) {
-
+    for (var i=0; i<arr.length; i++) {
       (function (i, arr) {
-        console.log(colors[i])
-
         var scrollWatch = arr[i],
-        watcher = scrollMonitor.create(scrollWatch, {top: -window.innerHeight/2, bottom: -window.innerHeight*2})
-
+        watcher = scrollMonitor.create(scrollWatch, { top: -window.innerHeight/2, bottom: -window.innerHeight/2 })
         scrollWatch.style.opacity = 1
 
         watcher.enterViewport(function () {
@@ -43,15 +31,11 @@ window.onload = () => {
             scrollWatch.querySelector('.project-intro').classList.remove('project-fadeout')
             scrollWatch.querySelector('.project-showcase').classList.remove('project-fadeout')
           }, 1500)
-
           currentLocation = window.scrollY
         })
 
         watcher.exitViewport(function () {
-          console.log('direction : ' + (currentLocation > window.scrollY ? 'UP' : 'DOWN'))
-
-          if (currentLocation < window.scrollY) {
-            console.log('up')
+          if (currentLocation <= window.scrollY) {
             scrollWatch.querySelector('.project-intro').classList.remove('project-fadein')
             scrollWatch.querySelector('.project-intro').classList.add('project-fadeout')
 
@@ -60,60 +44,15 @@ window.onload = () => {
               scrollWatch.querySelector('.project-showcase').classList.add('project-fadeout')
             }, 500)
           } else {
-            console.log('DOWN')
             scrollWatch.querySelector('.project-intro').classList.remove('project-fadein')
-            // scrollWatch.querySelector('.project-intro').classList.add('project-fadeout')
 
             setTimeout(function () {
               scrollWatch.querySelector('.project-showcase').classList.remove('project-fadein')
-              // scrollWatch.querySelector('.project-showcase').classList.add('project-fadeout')
             }, 500)
           }
-
-          // setTimeout(function() {
-          //   scrollWatch.querySelector('.project-intro').classList.remove('project-fadeout')
-          //   scrollWatch.querySelector('.project-showcase').classList.remove('project-fadeout')
-          // }, 2000)
-
           currentLocation = window.scrollY
         })
       })(i, arr)
     }
   }
-
-
-
-
-/*
-  var scrollWatch_cliqueMeet = document.querySelector('.clique-meet'),
-  watcher_cliqueMeet = scrollMonitor.create(scrollWatch_cliqueMeet, -500)
-
-  watcher_cliqueMeet.enterViewport(function () {
-    console.log('entered')
-
-    scrollWatch_cliqueMeet.querySelector('.project-intro').classList.add('project-fadein')
-
-    setTimeout(function () {
-      scrollWatch_cliqueMeet.querySelector('.project-showcase').classList.add('project-fadein')
-    }, 1000)
-
-  })
-
-  watcher_cliqueMeet.exitViewport(function () {
-    console.log('exit')
-
-    scrollWatch_cliqueMeet.querySelector('.project-intro').classList.remove('project-fadein')
-    scrollWatch_cliqueMeet.querySelector('.project-intro').classList.add('project-fadeout')
-
-    setTimeout(function () {
-      scrollWatch_cliqueMeet.querySelector('.project-showcase').classList.remove('project-fadein')
-      scrollWatch_cliqueMeet.querySelector('.project-showcase').classList.add('project-fadeout')
-    }, 500)
-
-    setTimeout(function() {
-      scrollWatch_cliqueMeet.querySelector('.project-intro').classList.remove('project-fadeout')
-      scrollWatch_cliqueMeet.querySelector('.project-showcase').classList.remove('project-fadeout')
-    }, 2000)
-  })
-*/
 }
