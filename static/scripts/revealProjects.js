ce4593400971810b5fa2a9c037805a131410a56c
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-09-13 07:56:24 +0530

    Re-arrange project reveal settings

--- static/scripts/revealProjects.js
+++ static/scripts/revealProjects.js
@@ -2,76 +2,78 @@ var colors = ['#61D97A', '#8125FB', '#61D97A', '#8125FB', '#61D97A', '#8125FB', 
 var currentLocation = window.scrollY
 
 callOnLoad = () => {
-  console.log('onload calling ')
-  var arr =  document.getElementsByClassName('project-details')
-  var projectPreview = document.querySelector('.project-preview')
-  var height = projectPreview.clientHeight
+  if (window.innerWidth >= 992) {
+    console.log('onload calling ')
+    var arr =  document.getElementsByClassName('project-details')
+    var projectPreview = document.querySelector('.project-preview')
+    var height = projectPreview.clientHeight
 
-  $('.project').fixer()
+    $('.project').fixer()
 
-  for (var i=0; i<arr.length; i++) {
-    ;(function (index, arr, colors) {
-      var scrollWatch = arr[index],
-      watcher = scrollMonitor.create(scrollWatch, { top: -window.innerHeight/2, bottom: -window.innerHeight/2 })
-      scrollWatch.style.opacity = 1
+    for (var i=0; i<arr.length; i++) {
+      ;(function (index, arr, colors) {
+        var scrollWatch = arr[index],
+        watcher = scrollMonitor.create(scrollWatch, { top: -window.innerHeight/2, bottom: -window.innerHeight/2 })
+        scrollWatch.style.opacity = 1
 
-      watcher.enterViewport(function () {
-        document.getElementById('Looper_Group').style.stroke = colors[index]
+        watcher.enterViewport(function () {
+          document.getElementById('Looper_Group').style.stroke = colors[index]
 
-        var animateImage = scrollWatch.getElementsByClassName('animate-image')
-        for (var i=0; i<animateImage.length; i++) {
-          if (animateImage[i].classList.contains('project-fadedown')) {
-            animateImage[i].classList.remove('project-fadedown')
-          }
-          if (animateImage[i].classList.contains('project-fadeup')) {
-            animateImage[i].classList.remove('project-fadeup')
-          }
-        }
-
-        scrollWatch.querySelector('.project-intro .hr-line').style.width = '50px'
-        scrollWatch.querySelector('.cover-inner').classList.add('right')
-
-        setTimeout(function () {
-          scrollWatch.querySelector('.view-case-study').style.opacity = 1
-        }, 1300)
-
-        var textRevealBlocks = scrollWatch.getElementsByClassName('init-animate-object')
-        for (var i=0; i<textRevealBlocks.length; i++) {
-          textRevealBlocks[i].classList.add('animate-object')
-        }
-
-        currentLocation = window.scrollY
-      })
-
-      watcher.exitViewport(function () {
-
-        var animateImage = scrollWatch.getElementsByClassName('animate-image')
-        if (currentLocation <= window.scrollY) {
+          var animateImage = scrollWatch.getElementsByClassName('animate-image')
           for (var i=0; i<animateImage.length; i++) {
-            animateImage[i].classList.add('project-fadeup')
+            if (animateImage[i].classList.contains('project-fadedown')) {
+              animateImage[i].classList.remove('project-fadedown')
+            }
+            if (animateImage[i].classList.contains('project-fadeup')) {
+              animateImage[i].classList.remove('project-fadeup')
+            }
           }
-        } else {
-          for (var i=0; i<animateImage.length; i++) {
-            animateImage[i].classList.add('project-fadedown')
+
+          scrollWatch.querySelector('.project-intro .hr-line').style.width = '50px'
+          scrollWatch.querySelector('.cover-inner').classList.add('right')
+
+          setTimeout(function () {
+            scrollWatch.querySelector('.view-case-study').style.opacity = 1
+          }, 1300)
+
+          var textRevealBlocks = scrollWatch.getElementsByClassName('init-animate-object')
+          for (var i=0; i<textRevealBlocks.length; i++) {
+            textRevealBlocks[i].classList.add('animate-object')
           }
-        }
 
-        currentLocation = window.scrollY
+          currentLocation = window.scrollY
+        })
 
-        scrollWatch.querySelector('.project-intro .hr-line').style.width = '0px'
-        scrollWatch.querySelector('.cover-inner').classList.remove('right')
+        watcher.exitViewport(function () {
 
-        setTimeout(function () {
-          scrollWatch.querySelector('.view-case-study').style.opacity = 0
-        }, 1300)
+          var animateImage = scrollWatch.getElementsByClassName('animate-image')
+          if (currentLocation <= window.scrollY) {
+            for (var i=0; i<animateImage.length; i++) {
+              animateImage[i].classList.add('project-fadeup')
+            }
+          } else {
+            for (var i=0; i<animateImage.length; i++) {
+              animateImage[i].classList.add('project-fadedown')
+            }
+          }
 
-        var textRevealBlocks = scrollWatch.getElementsByClassName('init-animate-object')
-        for (var i=0; i<textRevealBlocks.length; i++) {
-          textRevealBlocks[i].classList.remove('animate-object')
-        }
+          currentLocation = window.scrollY
 
-      })
-    })(i, arr, colors)
+          scrollWatch.querySelector('.project-intro .hr-line').style.width = '0px'
+          scrollWatch.querySelector('.cover-inner').classList.remove('right')
+
+          setTimeout(function () {
+            scrollWatch.querySelector('.view-case-study').style.opacity = 0
+          }, 1300)
+
+          var textRevealBlocks = scrollWatch.getElementsByClassName('init-animate-object')
+          for (var i=0; i<textRevealBlocks.length; i++) {
+            textRevealBlocks[i].classList.remove('animate-object')
+          }
+
+        })
+      })(i, arr, colors)
+    }
   }
 }
 
@@ -81,8 +83,8 @@ window.onscroll = () => {
   document.querySelector('.bg-looper > svg').style.transform = 'rotate(' + (window.scrollY / 180 / (Math.PI * 2)) + 'rad)'
 }
 
-if (window.innerWidth >= 992) {
+window.addEventListener('resize', () => {
+  callOnLoad ()
+})
 
-  window.onload = callOnLoad
-
-}
+window.onload = callOnLoad
