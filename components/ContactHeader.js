8f6b74ce9e67f7e42366f489ab1c36c673c60264
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-10-03 05:31:01 +0530

    add contact page header

---
+++ components/ContactHeader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NumieLogoRed } from './SVG/NumieLogo'
+
+export default class ContactHeader extends React.Component {
+  render () {
+    return (
+      <div className='contact-header'>
+        <div className='logo'>
+          <NumieLogoRed />
+        </div>
+        
+        <div className='close-button'>
+        </div>
+      </div>
+    )
+  }
+}
