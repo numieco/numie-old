75e4ea0e9a2cdafd2e92f90e070e28ff713bb7a5
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2017-10-03 05:31:49 +0530

    add contact page footer

---
+++ components/ContactFooter.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ArrowSVG from './SVG/Arrow'
+
+export default class ContactFooter extends React.Component {
+  constructor (props) {
+    super (props)
+  }
+
+  render () {
+    return (
+      <div className='contact-footer'>
+        <div className='nav-button prev'>
+          <div className='arrow'>
+            <ArrowSVG />
+          </div>
+        </div>
+        <div className='nav-button next disable'>
+          <div className='arrow'>
+            <ArrowSVG />
+          </div>
+        </div>
+        {/*
+          <div className='skip'>Skip</div>
+          <div className='nav-button send'>Send</div>
+        */}
+        <span className='press-enter'>
+          or press enter
+        </span>
+      </div>
+    )
+  }
+}
---
+++ components/SVG/Arrow.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ArrowSVG = () => (
+  <svg width="17px" height="9px" viewBox="0 0 17 9" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+      <title>Group 5 Copy</title>
+      <desc>Created with Sketch.</desc>
+      <defs></defs>
+      <g id="Contact" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+          <g id="Desktop---Contact---Budget" transform="translate(-667.000000, -679.000000)" stroke="#FFFFFF">
+              <g id="Group-10" transform="translate(580.000000, 658.000000)">
+                  <g id="Group-8">
+                      <g id="Group-7">
+                          <g id="Group-4" transform="translate(70.000000, 0.000000)">
+                              <g id="Group-5-Copy" transform="translate(18.000000, 21.000000)">
+                                  <path d="M0,4.5 L14.5454545,4.5" id="Shape"></path>
+                                  <polyline id="Shape" points="10.6363636 0 15 4.5 10.6363636 9"></polyline>
+                              </g>
+                          </g>
+                      </g>
+                  </g>
+              </g>
+          </g>
+      </g>
+  </svg>
+)
+
+export default ArrowSVG
